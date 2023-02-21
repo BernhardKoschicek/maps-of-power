e5fe6c66ee5e63071cdc66047cc06684149a0155
BernhardKoschicek <b.koschicek@gmail.com>
2023-02-21 15:45:53 +0100

    added some libraries and cleaned up

--- mop/static/setup.py
+++ mop/static/setup.py
@@ -2,9 +2,16 @@ from setuptools import setup
 
 package_json = {
     "dependencies": {
+        "@fortawesome/fontawesome-free": "^5.15.4",
+        "baguettebox.js": "1.11.1",
         "bootstrap-autocomplete": "2.3.7",
         "bootstrap": "^5.1.3",
-        "baguettebox.js": "1.11.1"
+        "datatables.net-buttons-dt": "^1.7.1",
+        "datatables.net-buttons": "^1.7.1",
+        "datatables.net-dt": "^1.11.5",
+        "datatables.net": "^1.11.5",
+        "jquery": "^3.6.0",
+        "jquery-ui-dist": "^1.13.1"
     }
 }
 
