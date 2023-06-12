b837144efeb89aebe4bdcd2c9925d6cef699c649
bkoschicek <b.koschicek@gmail.com>
2023-06-12 17:56:39 +0200

    added datatables library and further reading data

---
+++ mop/data/literature.py
@@ -0,0 +1,123 @@
+from flask_babel import lazy_gettext as _
+
+project_literature = [
+    {
+        'author':
+            'Mihailo St. Popović, Vratislav Zervan, Ralf C. Müller '
+            + _('eds.'),
+        'title':
+            'Der den Kordax tanzt: Hommage an Johannes Koder und seine '
+            'Forschungen über Byzanz und den Balkan',
+        'locations': [_('leipzig')],
+        'date': '',
+        'pages': '',
+        'link': '',
+        'category': ['mop']
+    }]
+
+further_reading = [
+    {
+        'author': 'Matthias Bürgi',
+        'title':
+            'Historische Ökologie – ein interdisziplinärer Forschungsansatz, '
+            'illustriert am Beispiel der Waldstreunutzung. GAIA: Ecological '
+            'Perspectives for Science and Society 17/4',
+        'locations': '',
+        'date': '2008',
+        'pages': '370-377',
+        'link': 'https://www.dora.lib4ri.ch/wsl/islandora/object/wsl%3A6545',
+        'category': ''
+    }, {
+        'author': 'Béatrice Caseau',
+        'title':
+            'Sacred Landscapes, in: Interpreting Late Antiquity. Essays on '
+            'the Postclassical World',
+        'locations': [_('cambridge, MA'), _('london')],
+        'date': '2001',
+        'pages': '21-59',
+        'link': '',
+        'category': ''
+    }, {
+        'author': 'Peter Howard, Ian Thompson, Emma Waterton, Mick Atha',
+        'title': 'The Routledge Companion to Landscape Studies',
+        'locations': [_('london'), _('new york')],
+        'date': '2020',
+        'pages': '',
+        'link': '',
+        'category': ''
+    }, {
+        'author': 'Graham Jones',
+        'title': 'Saints in the Landscape',
+        'locations': [_('stroud')],
+        'date': '2007',
+        'pages': '',
+        'link': '',
+        'category': ''
+    }, {
+        'author': 'Mihailo St. Popović',
+        'title': 'Historische Geographie und Digital Humanities. Eine '
+                 'Fallstudie zum spätbyzantinischen und osmanischen '
+                 'Makedonien (Peleus, Studien zur Archäologie und Geschichte '
+                 'Griechenlands und Zyperns 61)',
+        'locations': [_('mainz'), _('ruhpolding')],
+        'date': '2014',
+        'pages': '',
+        'link': '',
+        'category': ''
+    },{
+        'author': 'Mihailo St. Popović',
+        'title': 'Das Langzeitprojekt TIB im 21. Jahrhundert: '
+                 'Bestandsaufnahme, Adaptierung und Methodenerweiterung in '
+                 'der Historischen Geographie des byzantinischen Raumes, in: '
+                 'Raum und Geschichte: der historische Atlas „Tabula Imperii '
+                 'Byzantini“ an der Österreichischen Akademie der '
+                 'Wissenschaften (Studies in Historical Geography and '
+                 'Cultural Heritage 3).',
+        'locations': [_('vienna'), _('novi sad')],
+        'date': '2020',
+        'pages': '157-180, 231-238',
+        'link': '',
+        'category': ''
+    },{
+        'author': 'Mihailo St. Popović',
+        'title': 'A Race Against Time: The Impact of Contemporary '
+                 'Environmental and Demographic Changes on the Research of '
+                 'the Historical Geography of Byzantium, in: 24th '
+                 'International Congress of Byzantine Studies, Volume 1. '
+                 'Proceedings of the Plenary Sessions',
+        'locations': [_('venice')],
+        'date': '2022',
+        'pages': '191-210',
+        'link': 'http://doi.org/10.30687/978-88-6969-590-2/012',
+        'category': ''
+    },{
+        'author': 'Oliver Rackham',
+        'title': 'The History of the Countryside. The Classic History of '
+                 'Britain’s Landscape, Flora and Fauna',
+        'locations': [_('london')],
+        'date': '2020',
+        'pages': '',
+        'link': '',
+        'category': ''
+    },{
+        'author': 'Sam Turner',
+        'title': 'Making a Christian Landscape: The Countryside in '
+                 'Early-Medieval Cornwall, Devon and Wessex',
+        'locations': [_('exter')],
+        'date': '2006',
+        'pages': '',
+        'link': '',
+        'category': ''
+    },{
+        'author': 'Athanasios Vionis',
+        'title': 'Interdisciplinarity in Byzantine Studies. A '
+                 'Sacred-Landscapes and Digital-Humanities Approach, in: '
+                 '24th International Congress of Byzantine Studies, Volume 1. '
+                 'Proceedings of the Plenary Sessions',
+        'locations': [_('venice')],
+        'date': '2022',
+        'pages': '121-140',
+        'link': 'http://doi.org/10.30687/978-88-6969-590-2/008',
+        'category': ''
+    },
+]
