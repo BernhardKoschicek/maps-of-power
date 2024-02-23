86eeb330ebc37358fa9ec2f2357fda2430b6fe0f
Bernhard Koschicek-Krombholz <b.koschicek@gmail.com>
2024-02-23 15:17:15 +0100

    added proxy to request

--- mop/model/api_calls.py
+++ mop/model/api_calls.py
@@ -5,32 +5,41 @@ import requests
 from mop import app
 from mop.model.typetree import TypeTree
 
+PROXIES = {
+    "http": app.config['API_PROXY'],
+    "https": app.config['API_PROXY']}
+
 
 def get_view_class(parameter: str):
     url = f"{app.config['API_PATH']}/view_class/"
     return requests.get(
         f"{url}{parameter}",
-        proxies=app.config['PROXIES']).json()['results']
+        proxies=PROXIES,
+        timeout=30).json()['results']
 
 
 def system_class_results(parameter: str):
     url = f"{app.config['API_PATH']}/system_class/"
     return requests.get(
         f"{url}{parameter}",
-        proxies=app.config['PROXIES']).json()['results']
+        proxies=PROXIES,
+        timeout=30).json()['results']
 
 
 def get_typed_entities_all_results(id_: int):
     url = f"{app.config['API_PATH']}/type_entities_all/"
     return requests.get(
         f"{url}{id_}",
-        proxies=app.config['PROXIES']).json()['results']
+        proxies=PROXIES,
+        timeout=30).json()['results']
 
 
 def get_type_tree() -> List[TypeTree]:
     url = f"{app.config['API_PATH']}/type_tree/"
-    type_tree = requests.get(url, proxies=app.config['PROXIES']).json()[
-        'typeTree']
+    type_tree = requests.get(
+        url,
+        proxies=PROXIES,
+        timeout=30).json()['typeTree']
     return [TypeTree(types) for types in type_tree.values()]
 
 
@@ -39,17 +48,20 @@ def get_entities_linked_to_entity(id_: int, show: Optional[List[str]] = None):
     show_ = ''.join([f'&show={value}' for value in show] if show else '')
     return requests.get(
         f"{url}{id_}?limit=0{show_}",
-        proxies=app.config['PROXIES']).json()['results']
+        proxies=PROXIES,
+        timeout=30).json()['results']
 
 
 def get_entity(id_: int):
     url = f"{app.config['API_PATH']}/entity/"
     return requests.get(
         f"{url}{id_}",
-        proxies=app.config['PROXIES']).json()['features'][0]
+        proxies=PROXIES,
+        timeout=30).json()['features'][0]
 
 
 def api_call(url):
     return requests.get(
         url,
-        proxies=app.config['PROXIES']).json()['features'][0]
+        proxies=PROXIES,
+        timeout=30).json()['features'][0]
